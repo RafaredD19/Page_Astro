151f0c499b4b0d4a1fb417060ff4e4c7c4659051
rafael <rosselgalarzarafael@gmail.com>
2024-01-22 02:34:35 -0500

    xd2

--- .netlify/functions-internal/ssr/entry.mjs
+++ .netlify/functions-internal/ssr/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/ssr-function.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_K0UgrQ8O.mjs';
+import { manifest } from './manifest_lsGAOLUB.mjs';
 
 const _page0  = () => import('./chunks/generic_UB-VrW_P.mjs');
 const _page1  = () => import('./chunks/index_bvUm0Qp8.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1]]);
