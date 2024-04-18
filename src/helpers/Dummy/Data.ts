4d4c4630010ae769a87918ac6d1863d0ca335151
rosselgalarzarafael <rosselgalarzarafael@gmail.com>
2024-04-18 00:00:58 -0500

    cambios en mi resposive y actualización de información

--- src/helpers/Dummy/Data.ts
+++ src/helpers/Dummy/Data.ts
@@ -11,7 +11,7 @@ export type IDataExperiencie = {
   role: string;
   company: string;
   duraction: string;
-  address: string;
+  tool: string;
   aptitudes: string;
 }[]
 
@@ -31,8 +31,8 @@ export const dataRS: IDataRS = [
     icons: "LinkedIn",
   },
   {
-    name: "Tiktok",
-    link: "https://www.tiktok.com/@rafaelandresrosse?_t=8jEoDS0109W&_r=1",
+    name: "Gitlab",
+    link: "https://gitlab.com/rosselgalarzarafael",
     icons: "Youtube",
   },
   {
@@ -46,26 +46,26 @@ export const dataRS: IDataRS = [
 
 export const dataExperiencie: IDataExperiencie = [
   {
-  role: "Programador junior",
+  role: "Desarrollador Full Stack",
   company: "Sysnet Del Perú - jornada completa",
   duraction: "Nov. 2023 - actualidad ·  6 meses",
-  address: "Las Condes, Región Metropolitana de Santiago, ChileLas Condes, Región Metropolitana de Santiago, Chile",
-  aptitudes: "Desarrollo de páginas web · Desarrollode scripts de automatización (Python) · Creación de Base de datos(SQL Server, MySql)",
+  aptitudes: "·Desarrollo de plataformas SaaS · Desarrollo de scripts de automatización · Creacción de Bases de datos relacionales y no relacionales",
+  tool: "-Vue js -Express js -Nest Js -Flask -FastApi -Mongo DB -MySql -SQL serve ",
 },
 {
-  role: "Analista de datos",
+  role: "Analista de datos Junior",
   company: "San Fernando · Jornada completa",
   duraction: "Agos. 2022 - ene. 2023 · 6 meses",
-  address: "Región Metropolitana de Santiago, Chile",
-  aptitudes: "Creación de informes para la mejora continua(PowerBi, Excel, SAP)",
+  aptitudes: "Creación de informes para la mejora continua",
+  tool: "-PowerBi -SAP -Excel -Python",
 },
   
 {
-  role: "Desarrollador WEB",
+  role: "Desarrollador FrontEnd",
   company: "Opseli  · Jornada completa",
   duraction: "ene. 2022 - jul. 2022 · 7 meses",
-  address: "Región Metropolitana de Santiago, Chile",
-  aptitudes: "Desarrollo de páginas web(html, css, bootstrap, php, javascript) ",
+  aptitudes: "Desarrollo de páginas web y mantenimiento de sus plataformas",
+  tool: "-HTML -CSS -JavaScript -php -WordPress",
 },
 
 ]
@@ -75,6 +75,22 @@ export const dataExperiencie: IDataExperiencie = [
 export const dataProjects: IProject = [
   {
     id: 1,
+    title: 'Plataforma de cotizaciones de servicios',
+    img: 'img/oyt.png',
+    description: '',
+    url: 'http://204.48.17.106:3050/login',
+    active: 1
+  },
+  {
+    id: 5,
+    title: 'Plataforma para el control de despachos de combustible (integrado con un app movil)',
+    img: 'img/efuel.png',
+    description: '',
+    url: 'https://superlative-squirrel-f5fb60.netlify.app/',
+    active: 1
+  },
+  {
+    id: 2,
     title: 'Página personal(particle js)',
     img: 'img/blog_particle.png',
     description: '',
@@ -90,12 +106,5 @@ export const dataProjects: IProject = [
     url: 'https://opseli.com/',
     active: 1
   },
-  {
-    id: 4,
-    title: 'Página web de transportes',
-    img: 'img/oyt.png',
-    description: '',
-    url: 'http://www.oyt.com.pe/index.php',
-    active: 1
-  }
+ 
 ]
